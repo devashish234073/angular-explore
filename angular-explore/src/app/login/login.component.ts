24e97cdc846e69771f0af06009099c87513637f5
Devashish <devashish234073@gmail.com>
2022-01-13 14:06:01 +0530

    adding login form with validation

--- angular-explore/src/app/login/login.component.ts
+++ angular-explore/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { CustomValidators } from './custom.validators';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +12,19 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  username = new FormControl('', [Validators.required, CustomValidators.cannotContainSpace]);
+  password = new FormControl('', [Validators.required]);
+
+  loginStatus = "not done";
+
+  login() {
+    if(this.username.value!="devadeva" || this.password.value!="devadeva") {
+      this.loginStatus = "failed";
+    } else {
+      this.loginStatus = "success";
+    }
   }
 
 }
